9f1f6adbd35fec1fe08ebd7f2a199486b864190e
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2025-02-13 14:26:16 +0800

    1

--- web_ui/src/App.tsx
+++ web_ui/src/App.tsx
@@ -1,35 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
-import {
-  Route,
-  useNavigate,
-  useMatch,
-  Routes,
-  // useLocation
-} from "react-router-dom";
+import { Route, useMatch, Routes } from "react-router-dom";
 
 import ThemeProvider from "@components/ThemeProvider";
 import AppBar from "@components/appbar";
-import Home from "@components/home";
-import Config from "@components/config";
 import Preview from "@components/preview";
-import CalibrationNavigator, { ROUTE as CALIB_ROUTE } from "@components/calibration/Navigator";
 import DevConfig from "@components/dev";
-import Log from "@components/dev/Log";
-import FileUploader from "@components/upgrade";
-import MapEditor from "@components/editor";
-import AskMoney from "@components/general/AskMoney";
-import UserManager from "@components/general/UserManager";
-import TViz from "@components/tviz";
 import log from "loglevel";
-import { useLocalStorageState, useRequest, useSessionStorageState } from "ahooks";
+import { useLocalStorageState, useSessionStorageState } from "ahooks";
 import _ from "lodash";
-import { isPermit } from "@rpc/http";
 
 const USER_TABS = {
-  config: Config,
-  preview: Preview,
-  calibration: CalibrationNavigator,
-  tviz: TViz,
+  // config: Config,
+  // preview: Preview,
+  // calibration: CalibrationNavigator,
+  // tviz: TViz,
 };
 const UPGRADE_TABS = {
   // upgrade: FileUploader,
@@ -43,28 +27,11 @@ export default function App() {
   const [isDeveloperMode, setIsDeveloperMode] = useSessionStorageState("devMode", false);
   const [isDarkMode, setIsDarkMode] = useLocalStorageState("theme", false);
   const [menuBars, setMenuBar] = useState(<div />);
-  const [error, setError] = useState<Error>();
   const editorRef = useRef<any>();
-  const navigate = useNavigate();
   let url = useMatch("/:item");
-  const { data, run, cancel } = useRequest(isPermit, {
-    pollingInterval: 10000,
-    loadingDelay: 1000,
-    onSuccess: (data) => {
-      if (data.licence == "registered") {
-        cancel();
-      } else if (data.licence == "error" && url?.pathname != "/licence") {
-        navigate("/licence", { replace: true });
-      }
-    },
-  });
 
   const userLinks =
-    url?.pathname == "/editor"
-      ? []
-      : error
-      ? ["home", "upgrade"]
-      : _.concat("home", Object.keys(USER_TABS), Object.keys(UPGRADE_TABS));
+    url?.pathname == "/editor" ? [] : _.concat("home", Object.keys(USER_TABS), Object.keys(UPGRADE_TABS));
 
   const developerLinks = url?.pathname == "/editor" ? [] : Object.keys(DEVELOPER_TABS);
 
@@ -82,7 +49,6 @@ export default function App() {
     }
   }, [editorRef, url]);
 
-  let mapEditor = React.useMemo(() => <MapEditor ref={editorRef} />, [editorRef]);
 
   return (
     <ThemeProvider isDarkMode={isDarkMode}>
@@ -99,19 +65,7 @@ export default function App() {
       />
       <Routes>
         <Route path="/">
-          <Route index element={<Home onSuccess={() => setError(undefined)} />} />
-          <Route path="home" element={<Home onSuccess={() => setError(undefined)} />} />
-          <Route path="licence" element={<AskMoney />} />
-          <Route path="users" element={<UserManager />} />
-          <Route path="editor" element={mapEditor} />
-          {Object.entries({
-            ...USER_TABS,
-            ...UPGRADE_TABS,
-            ...DEVELOPER_TABS,
-          }).map((kv) => {
-            const [name, EL] = kv;
-            return <Route key={name} path={`/${name}/*`} element={<EL />} />;
-          })}
+          <Route index element={<Preview />} />
         </Route>
       </Routes>
     </ThemeProvider>
